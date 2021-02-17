5704b64886e8726a057a148eafb8ec720da9484b
Farai Motsi <farai.motsi@gmail.com>
2021-02-17 22:08:28 +0200

    added mvc artifacts for songs

---
+++ Songlist/Songlist/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Songlist.Models;
+
+namespace Songlist.Controllers
+{
+    public class HomeController : Controller
+    {
+     private SongContext context { get; set; }
+
+        public HomeController(SongContext ctx )
+        {
+            context = ctx;           
+        }
+
+        public IActionResult Index()
+        {
+            var songs = context.Songs.OrderBy(m => m.Name).ToList();
+            return View(songs);
+        }
+
+    }
+}
---
+++ Songlist/Songlist/Models/SongContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Songlist.Models
+{
+    public class SongContext : DbContext
+    {
+        public SongContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Song> Songs { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Song>().HasData(
+
+                new Song
+                {
+                    SongId = 1,
+                    Name = "Master of Puppets",
+                    Year = 1980,
+                    Rating = 5
+                },
+                new Song
+                {
+                    SongId = 2,
+                    Name = "Wonderwall",
+                    Rating = 4,
+                    Year = 1990
+                },
+                 new Song
+                 {
+                     SongId = 3,
+                     Name = "Lose Yourself",
+                     Rating = 1,
+                     Year = 2005
+                 }
+                );
+        }
+    }
+}
